6c4391fdcf1b4a2577c53774e13a689ce41181bb
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-12-12 23:29:21 -0500

    Update mouseSDL.c

--- mouseSDL.c
+++ mouseSDL.c
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main() {
+int main() { 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
